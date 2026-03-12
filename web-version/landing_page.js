89affdea483adb7a0a6b7051f15767e5a3748280
victordessaune <victorramosdessaune@gmail.com>
2026-03-12 18:26:04 -0300

    Alterações na Landing Page: navbar finalizada, diferencias do desbravando iniciados

---
+++ web-version/landing_page.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function () {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } 
+    else {
+        navbar.classList.remove("scrolled");
+    }
+
+});

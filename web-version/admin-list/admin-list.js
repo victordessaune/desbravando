6f102b885d7691df53a6b853b11af2eee6b3824f
amandafilg <amandafilgueiras07@gmail.com>
2026-04-30 18:29:48 -0300

    Backend dos administrador

--- web-version/admin-list/admin-list.js
+++ web-version/admin-list/admin-list.js
@@ -1,3 +1,7 @@
+import { db, auth } from "../js/api/firebase.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 // ── POPUP ──
 window.openPopup  = () => document.getElementById('popup-responsavel').classList.add('open');
 window.closePopup = () => document.getElementById('popup-responsavel').classList.remove('open');
@@ -57,4 +61,53 @@ function showErr(id, msg) {
     document.getElementById('err-' + id).textContent = msg;
     document.getElementById('err-' + id).classList.add('show');
     document.getElementById(id)?.classList.add('err');
+}
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const userId = user.uid;
+    loadData(userId);
+  } else{
+    console.log("Usuário não logado.")
+  }
+});
+
+async function loadData(uid){
+  console.log("UID logado:", uid);
+  
+  const docRef = doc(db,"users", uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()){
+    const data = docSnap.data();
+    console.log("Nome:", data.firstName);
+    const personName = `${data.firstName || ""} ${data.lastName || ""}`;
+    const userLetters = getInitials(personName);
+    /*const createdDate = data.createdAt.toDate();
+    const signinDate = createdDate.toLocaleDateString("pt-BR");*/
+
+    document.getElementById("admin-email").textContent = data.email;
+    document.getElementById("admin-occupation").textContent = data.occupation;
+    document.getElementById("admin-name").textContent = personName;
+    document.getElementById("icon-user").textContent = userLetters;
+    /*document.getElementById("admin-date").textContent = signinDate;*/
+  
+  function getInitials(name) {
+  const ignore = ["de", "da", "do", "dos", "das"];
+
+  const words = name
+    .split(" ")
+    .filter(p => p.trim() !== "");
+
+  if (words.length === 1) {
+    return words[0].substring(0, 2).toUpperCase();
+  }
+
+  return words
+    .filter(p => !ignore.includes(p.toLowerCase()))
+    .map(p => p[0].toUpperCase())
+    .slice(0, 2)
+    .join("");
+}
+}
 }

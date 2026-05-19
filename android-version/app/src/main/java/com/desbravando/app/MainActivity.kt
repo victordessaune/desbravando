330acde34f4c2185b180c5fb8fc507b665888ad9
amandafilg <amandafilgueiras07@gmail.com>
2026-05-19 16:31:37 -0300

    Testes para aprender a usar o Jetpack Compose

--- android-version/app/src/main/java/com/desbravando/app/MainActivity.kt
+++ android-version/app/src/main/java/com/desbravando/app/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Color
+import com.desbravando.app.ui.theme.Pink40
+import com.desbravando.app.ui.theme.Purple40
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.desbravando.app.ui.theme.DesbravandoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +30,42 @@ class MainActivity : ComponentActivity() {
         setContent {
             DesbravandoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Login(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Login(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Pink40),
+        verticalArrangement = Arrangement.Bottom,
+    ) {
+        Text(
+            text = "Desbravando",
+            color = Color.White,
+        )
+        Surface(
+            modifier = modifier
+                .size(500.dp)
+                .background(color = Color.White)
+        ) {
+            Text(
+                text = "Login",
+            )
+        }
+
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoginPreview() {
     DesbravandoTheme {
-        Greeting("Android")
+        Login()
     }
 }

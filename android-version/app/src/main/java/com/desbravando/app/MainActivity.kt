dc95aa5897d0b808fe6729a3d223317dea9a75b2
amandafilg <amandafilgueiras07@gmail.com>
2026-05-20 16:37:54 -0300

    Início do desenvolvimento da tela de Login

--- android-version/app/src/main/java/com/desbravando/app/MainActivity.kt
+++ android-version/app/src/main/java/com/desbravando/app/MainActivity.kt
@@ -9,26 +9,32 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BrushPainter
 import androidx.compose.ui.res.painterResource
-import com.desbravando.app.ui.theme.Pink40
-import com.desbravando.app.ui.theme.Purple40
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.desbravando.app.ui.theme.BlueSecondary
 import com.desbravando.app.ui.theme.DesbravandoTheme
+import com.desbravando.app.ui.theme.LightPurple
+import com.desbravando.app.ui.theme.Purple
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,38 +52,66 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Login(modifier: Modifier = Modifier) {
     Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxSize()
-            .background(Pink40),
-        verticalArrangement = Arrangement.Bottom,
-    ) {
-        Text(
-            text = "Desbravando",
-            color = Color.White,
-        )
-        Image(
-            painter = painterResource(id = R.drawable.white_logo_desbravando),
-            contentDescription = "Logo",
-            modifier = Modifier
-                .width(180.dp)
-                .height(60.dp)
-        )
+            .background(
+                brush = Brush.verticalGradient(
+                    colorStops = arrayOf(
+                        0.0f to Purple,
+                        0.48f to BlueSecondary
+                    )
+                )
+            ),
 
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.white_logo_desbravando),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(60.dp)
+
+            )
+        }
         Surface(
-            modifier = Modifier.size(500.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(500.dp),
             shape = RoundedCornerShape(
                 topStart = 32.dp,
                 topEnd = 32.dp
             ),
             color = Color.White
         ) {
-            Text(
-                text = "Login",
-                modifier = Modifier
-                    .padding(
-                        top = 32.dp,
-                        start = 24.dp)
-            )
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(24.dp),
+
+            ) {
+                Text(
+                    text = "Login",
+                    fontSize = 18.sp,
+                    color = Purple,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Spacer(
+                    modifier = Modifier.width(10.dp)
+                )
+                Text(
+                    text = "Usuário",
+                    fontSize = 18.sp,
+                    color = BlueSecondary,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+            }
         }
     }
 }
--- android-version/app/src/main/java/com/desbravando/app/ui/theme/Color.kt
+++ android-version/app/src/main/java/com/desbravando/app/ui/theme/Color.kt
@@ -6,6 +6,25 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
 val Pink40 = Color(0xFF7D5260)
+
+val Purple = Color(0xFF4B3CC3)
+val LightPurple = Color(0xFFC48FCE)
+
+val Blue = Color(0xFF5170FF)
+val BlueSecondary = Color(0xFF7AA8FF)
+val LightBlue = Color(0xFF6EE7F9)
+val DarkBlue = Color(0xFF080630)
+
+val White = Color(0xFFFFFFFF)
+
+val WhiteRgb = Triple(255, 255, 255)
+
+val LightGray = Color(0xFFEAEAEA)
+val DarkGray = Color(0xFF6B7280)
+val Gray = Color(0xFF9CA3AF)
+val MediumGray = Color(0xFFC4C9D4)
+
+val OffWhite = Color(0xFFF4F5FB)

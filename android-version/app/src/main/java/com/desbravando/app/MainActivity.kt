f364e0f3d13d7535f83673b45437eee69919738d
amandafilg <amandafilgueiras07@gmail.com>
2026-05-20 14:40:28 -0300

    Mias tentativas de aprender a usar o Jetpack Compose - a logo foi adicionada

--- android-version/app/src/main/java/com/desbravando/app/MainActivity.kt
+++ android-version/app/src/main/java/com/desbravando/app/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import com.desbravando.app.ui.theme.Pink40
 import com.desbravando.app.ui.theme.Purple40
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,17 +55,30 @@ fun Login(modifier: Modifier = Modifier) {
             text = "Desbravando",
             color = Color.White,
         )
+        Image(
+            painter = painterResource(id = R.drawable.white_logo_desbravando),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .width(180.dp)
+                .height(60.dp)
+        )
+
         Surface(
-            modifier = modifier
-                .size(500.dp)
-                .background(color = Color.White)
+            modifier = Modifier.size(500.dp),
+            shape = RoundedCornerShape(
+                topStart = 32.dp,
+                topEnd = 32.dp
+            ),
+            color = Color.White
         ) {
             Text(
                 text = "Login",
+                modifier = Modifier
+                    .padding(
+                        top = 32.dp,
+                        start = 24.dp)
             )
         }
-
-
     }
 }
 

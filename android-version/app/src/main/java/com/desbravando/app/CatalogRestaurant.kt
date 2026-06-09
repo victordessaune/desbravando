63e3981182231859481c36fc899740b9440a82ac
amandafilg <amandafilgueiras07@gmail.com>
2026-06-08 23:43:57 -0300

    Alguns pequenos avanços na página de catálogo

--- android-version/app/src/main/java/com/desbravando/app/CatalogRestaurant.kt
+++ android-version/app/src/main/java/com/desbravando/app/CatalogRestaurant.kt
@@ -1,7 +1,5 @@
 package com.desbravando.app
 
-import android.R.attr.fontWeight
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,90 +7,176 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.desbravando.app.ui.theme.Blue
-import com.desbravando.app.ui.theme.BlueSecondary
 import com.desbravando.app.ui.theme.DesbravandoTheme
-import com.desbravando.app.ui.theme.NavGraph
 import com.desbravando.app.ui.theme.OffWhite
 import com.desbravando.app.ui.theme.Poppins
-import com.desbravando.app.ui.theme.Purple
+import com.desbravando.app.ui.theme.White
+import com.google.android.gms.ads.nativead.NativeAd.Image
 
 class CatalogRestaurant : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
-        setContent {
 
-        }
+        setContent {
+            DesbravandoTheme {
+                Catalog()
+            }
         }
     }
+}
 
 @Composable
-fun Catalog(
-) {
+fun Catalog() {
+
+    var search by remember {mutableStateOf("")}
+
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(15.dp)
+            .fillMaxSize()
+            .background(OffWhite)
     ) {
-        Row() {
-            Image(
-                painter = painterResource(id = R.drawable.ic_back),
-                contentDescription = "Logo",
-                modifier = Modifier
-                    .width(28.dp)
-                    .height(28.dp)
-                    .border(width = 1.dp, color = Blue, shape = RoundedCornerShape(4.dp))
-                    .padding(5.dp),
+        Column(
+            modifier = Modifier.padding(
+                top = 40.dp,
+                start = 20.dp,
+                end = 20.dp
             )
-            Spacer(modifier = Modifier.width(6.dp))
+        ) {
 
-            Image(
-                painter = painterResource(id = R.drawable.restaurant_icon),
-                contentDescription = "Logo",
-                modifier = Modifier
-                    .width(25.dp)
-                    .height(25.dp)
+            Row {
+
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_back),
+                    contentDescription = "Voltar",
+                    tint = Blue,
+                    modifier = Modifier
+                        .size(28.dp)
+                        .border(
+                            width = 1.dp,
+                            color = Blue,
+                            shape = RoundedCornerShape(4.dp)
+                        )
+                        .padding(5.dp)
+                )
+
+                Spacer(modifier = Modifier.width(6.dp))
+
+                Icon(
+                    painter = painterResource(id = R.drawable.restaurant_icon),
+                    contentDescription = "Ícone de restaurante"
+                )
+
+                Spacer(modifier = Modifier.width(5.dp))
+
+                Text(
+                    text = "GastroBar",
+                    fontFamily = Poppins,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+        }
+        
+        HorizontalDivider(
+            thickness = 1.dp
+        )
+
+        Column(
+            modifier = Modifier.padding(
+                horizontal = 20.dp,
+                vertical = 15.dp
             )
-            Spacer(modifier = Modifier.width(5.dp))
+        ) {
+
+            OutlinedTextField(
+                value = search,
+                onValueChange = {
+                    search = it
+                },
+                placeholder = {
+                    Text("Pesquisar restaurante")
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Pesquisar"
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp),
+                singleLine = true
+            )
+        }
+        
+        HorizontalDivider(
+            thickness = 1.dp
+        )
+
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(100.dp)
+                    .fillMaxWidth()
+                    .background(
+                        color = White,
+                        shape = RoundedCornerShape(16.dp)
+                    )
+            ){
+                Row(
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.cb3),
+                        contentDescription = "Foto do lugar",
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Column() {
+                        Text(
+                            text = "Nome do Lugar",
+                            fontFamily = Poppins,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                        Text(
+                            text = "Localização",
+                            fontFamily = Poppins,
+                            fontSize = 18.sp,
+                        )
+                    }
+                }
+            }
 
             Text(
-                text = "GastroBar",
+                text = "Restaurantes",
                 fontFamily = Poppins,
                 fontSize = 18.sp,
-                fontWeight = FontWeight(500),
+                fontWeight = FontWeight.SemiBold
             )
 
         }
-        Spacer(modifier = Modifier.width(6.dp))
-
-        Row() { }
     }
 }
 
